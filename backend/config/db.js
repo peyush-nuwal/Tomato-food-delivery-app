07c0b396347a399800cb87d7fb35429de77b4717
peyush <piyushnawal19.com>
2024-06-25 16:42:45 +0530

    check2

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,7 @@ import mongoose from 'mongoose';
 export const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+           
         });
         console.log("Database connected successfully");
     } catch (err) {
